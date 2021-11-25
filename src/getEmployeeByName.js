cbc22d860f6790c029ed3ef677c41f025ee5bd82
Alex Silva <alexcssilva@gmail.com>
2021-11-24 23:00:39 -0300

    Desafio 04 concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,15 +1,15 @@
-const { employees } = require("../data/zoo_data");
-const data = require("../data/zoo_data");
+const { employees } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   const emploName = employees.find(
     (employee) =>
-      employee.firstName === employeeName || employee.lastName === employeeName
+      employee.firstName === employeeName || employee.lastName === employeeName,
   );
   if (emploName === undefined) {
     return {};
   }
   return emploName;
 }
-console.log(getEmployeeByName());
+
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
-const data = require('../data/zoo_data');
+const { employees } = require("../data/zoo_data");
+const data = require("../data/zoo_data");
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((manager) => manager.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const subordinates = employees.filter((employee) =>
+      employee.managers.some((x) => x === managerId));
+    return subordinates.map(
+      (subord) => `${subord.firstName} ${subord.lastName}`,
+    );
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
+
+// Jonatas Lima - Turma 17 e Willian Alves - Turma 17
