f4c1c93c9bcc00b9ee00081de52396f18fd39be8
Alex Silva <alexcssilva@gmail.com>
2021-11-25 22:07:50 -0300

    Desafio 06 requisito 1 concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,35 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach((person) => {
+    if (person.age < 18) {
+      child += 1;
+    } else if (person.age >= 18 && person.age < 50) {
+      adult += 1;
+    } else {
+      senior += 1;
+    }
+  });
+  return {
+    child,
+    adult,
+    senior,
+};
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
 }
-
+console.log(countEntrants([
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+]
+));
 module.exports = { calculateEntry, countEntrants };
