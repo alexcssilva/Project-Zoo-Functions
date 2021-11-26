e2a201a6e6a6979e01747487a3dfc0721236f999
Alex Silva <alexcssilva@gmail.com>
2021-11-26 16:35:13 -0300

    Desafio 09 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeerId = employees.find((employeer) => employeer.id === id).responsibleFor[0];
+  const specieId = species.find((specie) => specie.id === employeerId).residents;
+  const specieAndEmployeer = specieId.sort((a, b) => b.age - a.age)[0];
+  const { name, sex, age } = specieAndEmployeer;
+
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+// Auxiliado por George Lucas - Turma 17 e Willian Alves - Turma 17
